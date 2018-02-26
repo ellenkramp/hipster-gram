50e2d3e262ae2ed2029825321ffdd1650e987edc
Ellen Kramp <ellenkramp@Ellens-MacBook-Pro.local>
2018-02-26 12:42:27 -0500

    refactoringto take element creation out of javascript and into html file

--- instagram.js
+++ instagram.js
@@ -1,20 +1,10 @@
 var body = document.querySelector("body");
-var profile = document.createElement("div");
-var galleryDiv = document.createElement("div");
-var lightBoxDiv = document.createElement("div");
+var lightBoxDiv = document.querySelector("lightBox");
+var gallery = document.querySelector('gallery');
 var lightBoxImg = document.createElement("img");
-lightBoxDiv.classList.add("enlarged");
-galleryDiv.classList.add("gallery-class");
-
 var profilePic = document.createElement('img');
-body.appendChild(profile);
-profile.appendChild(profilePic);
-profile.classList.add('profile');
-profileHeader = document.createElement("h1");
-profile.appendChild(profileHeader);
-// profileHeader.textcontent("Hello World");
-
 profilePic.src = 'https://s-media-cache-ak0.pinimg.com/avatars/misskramp_1349735122_280.jpg';
+
 var gallery = [
     {link: 'http://www.lovethispic.com/uploaded_images/35108-Pretty-Halloween-Bokeh.jpg'},
     {link: 'http://www.our3dvr.com/data/wallpapers/64/WDF_1097626.jpg'},
@@ -23,12 +13,9 @@ var gallery = [
     {link: 'http://natbg.com/wp-content/uploads/2016/06/flowers-cotton-flower-pretty-nature-wallpaper-for-desktop.jpg'},
     {link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa9JNgXoDsIiCklFf4_hhUj_8CkkPjbDFERv-m-vrieo_9WZQbrw'}
 ];
-body.appendChild(profile);
-body.appendChild(lightBoxDiv);
-body.appendChild(galleryDiv);
-lightBoxDiv.appendChild(lightBoxImg);
 
 gallery.forEach(function(image) {
+    var galleryDiv = document.querySelector('gallery');
     var galImg = document.createElement('img');
     galImg.src = image.link;
     galleryDiv.appendChild(galImg);
@@ -45,3 +32,5 @@ function lightBox() {
 function normalState() {
     lightBoxDiv.classList.toggle('visible');
 }
+
+lightBoxDiv.appendChild(lightBoxImg);
